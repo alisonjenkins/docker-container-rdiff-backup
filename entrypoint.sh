dff8a39b87f97d23535bbe53b76170803d64ce4d
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-11-17 21:02:45 +0000

    Fix 'say' commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 while true; do
     sleep "$BACKUP_INTERVAL"
-    rconc 127.0.0.1:25575 "Backing up server"
     rconc 127.0.0.1:25575 save-all
+    rconc 127.0.0.1:25575 "say Backing up server"
     sleep 2
     rdiff-backup backup "$WORLD_PATH" "$BACKUP_PATH"
-    rconc 127.0.0.1:25575 "Backup completed"
+    rconc 127.0.0.1:25575 "say Backup completed"
 done
