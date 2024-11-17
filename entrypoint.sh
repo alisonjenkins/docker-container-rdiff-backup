ec35d012f7de8fce5ef00ce5242c874827203cf7
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-11-17 21:02:58 +0000

    Ensure save-all flushes to disk

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 while true; do
     sleep "$BACKUP_INTERVAL"
-    rconc 127.0.0.1:25575 save-all
     rconc 127.0.0.1:25575 "say Backing up server"
+    rconc 127.0.0.1:25575 "save-all flush"
     sleep 2
     rdiff-backup backup "$WORLD_PATH" "$BACKUP_PATH"
     rconc 127.0.0.1:25575 "say Backup completed"
