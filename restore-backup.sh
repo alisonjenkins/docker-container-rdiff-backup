8e7ae1cdcbe1face972d14fab147eda9beb0fa58
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-11-17 21:47:47 +0000

    Fix restore command

--- restore-backup.sh
+++ restore-backup.sh
@@ -2,7 +2,7 @@
 set -e
 rconc 127.0.0.1:25575 "say Restoring backup... stopping world saving..."
 rconc 127.0.0.1:25575 "save-off"
-rdiff-backup --restore-as-of "$1" --force /mnt/backups
+rdiff-backup --api-version 201 --restore-as-of "$1" --force "$BACKUP_PATH" "$WORLD_PATH"
 rconc 127.0.0.1:25575 "say Backup restored..."
 sleep 2
 rconc 127.0.0.1:25575 "say Restarting server..."
