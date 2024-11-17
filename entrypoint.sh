4cdfb0f623de5803f2ca12f522a25c5136d3b0c5
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-11-17 20:50:09 +0000

    Make entrypoint script use envvars again

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 while true; do
-    sleep 900
+    sleep "$BACKUP_INTERVAL"
     rconc 127.0.0.1:25575 "Backing up server"
     rconc 127.0.0.1:25575 save-all
     sleep 2
-    rdiff-backup backup /mnt/state /mnt/backups
+    rdiff-backup backup "$WORLD_PATH" "$BACKUP_PATH"
     rconc 127.0.0.1:25575 "Backup completed"
 done
